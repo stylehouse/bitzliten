7dfe7c38b50e78dd8826cd96c689419244ed4adf
Steve <ostylehouse@gmail.com>
2024-03-26 21:07:10 +1300

    how to configure Cross-Origin-(Opener|Embedder)-Policy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+const viteServerConfig = {
+    name: 'log-request-middleware',
+    configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET");
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            next();
+        });
+    }
+};
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), viteServerConfig],
 	optimizeDeps: {
 		exclude: ["@ffmpeg/ffmpeg","@ffmpeg/util"]
 	},
 	ssr: {
 		noExternal: ['@ffmpeg/util']
 	},
-	server: {
-	  headers: {
-		"Cross-Origin-Opener-Policy": "same-origin",
-		"Cross-Origin-Embedder-Policy": "require-corp",
-	  },
-	},
+	// we don't seem to need these, see 'server.middlewares' above
+	// server: {
+	//   headers: {
+	// 	"Cross-Origin-Opener-Policy": "same-origin",
+	// 	"Cross-Origin-Embedder-Policy": "require-corp",
+	//   },
+	// },
 });
