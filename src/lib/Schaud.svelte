276f72f5770d593b26416b9c3661833f1ac76782
Steve <ostylehouse@gmail.com>
2024-04-09 13:03:00 +1200

    better looking

--- src/lib/Schaud.svelte
+++ src/lib/Schaud.svelte
@@ -85,12 +85,14 @@
         cuelet.intime = cuelet.in - sel.in;
         cuelet.outtime = cuelet.out - sel.in;
     }
+    // convert objectURL to buffer
     async function decodeAudio(cuelet) {
         if (!cuelet.objectURL) return
         let buf = await fetch(cuelet.objectURL)
         // our fetch() returns a Uint8Array!
         let res = new Response(buf)
         let blob = await res.arrayBuffer()
+        // we can start playing cuelets before they all have:
         cuelet.buffer = await audioContext.decodeAudioData(blob)
     }
     // handles wraparound
@@ -109,19 +111,63 @@
         if (cuelets[0]?.buffer) ready = 1
     })
     $effect(() => {
-        if (ready) scheduleNextSound(cuelets[0])
+        if (ready) scheduleNextSound()
     })
+    let fadetime = 0.5
     let upto = $state(0)
+    let cuenow:null|adublet = null
     let theone = {}
-    function scheduleNextSound(cuelet:adublet,the) {
+    // we shall run this whenever a new cuelet needs to .source and .play()
+    //  also runs at random other times
+    function scheduleNextSound(the) {
+        // dedupe callbacks to here
         if (the && the != theone) return
         the = theone = {}
-        upto = cuelet.in
-        console.log(`NextSound: ${cuelet.in}  ${cuelet.buffer.length}`)
+        // on init
+        cuenow ||= cuelets[0]
+        if (!cuenow) throw "!cuenow"
+        // one of these while playing
+        if (!cuenow.source) {
+            let source = cuenow.source = source_cuelet(cuenow)
 
+            // play
+            source.start(audioContext.currentTime);
+
+            // crossfade
+            let cuenext = next_cuelet(cuenow)
+            if (cuenext.in < cuenow.out) {
+                // < how to get to the time to do it more reliably?
+                let left = source.buffer.duration - fadetime
+                setTimeout(() => {
+                    
+                },left)
+            }
+
+            source.onended = () => {
+                delete cuenow.source
+                // cuenext will already be playing (has .source) if crossfading
+                cuenow = cuenext
+                scheduleNextSound(the);
+            };
+        }
+
+        let cuelet = cuenow
+        upto = cuelet.in
+        
+
+        console.log(`NextSound: ${cuelet.in}  ${cuelet.buffer.duration}`)
+
+
+        console.log("Zourece",{
+            fadein: cuenow.source.fadein(5),
+            sourcetime: cuenow.source.currentTime,
+            time: audioContext.currentTime,
+        })
+
+    }
+    function source_cuelet(cuelet:adublet) {
         let source = audioContext.createBufferSource();
         source.buffer = cuelet.buffer;
-        // let source = oscil()
 
 
         // Create a gain node to control the volume
@@ -129,21 +175,17 @@
         source.connect(gainNode);
         gainNode.connect(audioContext.destination);
 
-        source.start(audioContext.currentTime);
-        source.onended = () => {
-            let cuenext = next_cuelet(cuelet)
-            if (cuenext.in < cuelet.out) {
-                console.log("Wrap")
-            }
-            scheduleNextSound(cuenext,the);
-        };
+        // < fades
+        source.fadein = (n) => n * 3
+
+        return source
     }
     function oscil() {
         const oscillator = audioContext.createOscillator();
         // Set the oscillator type to sine wave
         oscillator.type = 'sine';
         // Set the frequency to 60Hz
-        oscillator.frequency.value = 60;
+        oscillator.frequency.value = 260;
         return oscillator
     }
 </script>
